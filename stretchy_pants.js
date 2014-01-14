d319f229220b16c464fa770981cb91f2122794a0
Dan Hixon <danhixon@gmail.com>
2014-01-13 21:12:07 -0700

    now works for multiple images

--- stretchy_pants.js
+++ stretchy_pants.js
@@ -23,8 +23,16 @@ jQuery.fn.calculateNaturalAspectRatio = function(callBack) {
 		Fills hero container with the hero image centered in the container.
 */
 jQuery.fn.stretchyPants = function(options) {
-	var image = $(this);
-
+	// There is probably a way cooler jQuery way to make this 
+	// work with multiple images but this works for now.
+	if($(this).size() > 1)
+	{
+		$(this).each(function(ix, img){
+			$(img).stretchyPants(options);
+		});
+		return $(this);
+	}
+	var image = $(this)
 	var data = image.data();
 
 	var default_options = {
@@ -48,22 +56,22 @@ jQuery.fn.stretchyPants = function(options) {
 		var container = data.options.container;
 		var container_aspect_ratio = container.width() / container.height();
 
-		if(window.hero_image_aspect_ratio) {
-			if(window.hero_image_aspect_ratio > container_aspect_ratio) {
+		if(data.hero_image_aspect_ratio) {
+			if(data.hero_image_aspect_ratio > container_aspect_ratio) {
 			// image is more wide than container:
-			var stretched_width = window.hero_image_natural_width * (container.height() / window.hero_image_natural_height)
+			var stretched_width = data.hero_image_natural_width * (container.height() / data.hero_image_natural_height)
 			var offset = '-' + (stretched_width-container.width())/2.0 + 'px';
 			image.css({ 'height': '100%', 'width': 'auto', 'left': offset, 'top': '0px' });
 		} else { // image is more tall than container:
-			var stretched_height = window.hero_image_natural_height * (container.width() / window.hero_image_natural_width)
+			var stretched_height = data.hero_image_natural_height * (container.width() / data.hero_image_natural_width)
 			var offset = '-' + (stretched_height-container.height())/2.0 + 'px';
 			image.css({ 'width': '100%', 'height': 'auto', 'top': offset, 'left': '0px' });
 		}
 		} else {
 			$(image).calculateNaturalAspectRatio(function(image){
-				window.hero_image_natural_width = image.naturalWidth;
-				window.hero_image_natural_height = image.naturalHeight;
-				window.hero_image_aspect_ratio = image.naturalWidth / image.naturalHeight;
+				data.hero_image_natural_width = image.naturalWidth;
+				data.hero_image_natural_height = image.naturalHeight;
+				data.hero_image_aspect_ratio = image.naturalWidth / image.naturalHeight;
 				// call this function again ... since the window may not have resized... curse you async!
 				setTimeout(stretchImage, 100);
 			})
@@ -77,6 +85,7 @@ jQuery.fn.stretchyPants = function(options) {
 	window.addEventListener('orientationchange', function(){
 		stretchImage();
 	});
-	stretchImage()
+	stretchImage();
+	return $(this);
 }
 
